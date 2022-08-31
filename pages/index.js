4737859306e27c93782d5899e6b99c9f5da86985
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2022-08-31 13:24:57 -0300

    New Update

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,11 @@ import {
   InputGroup,
   Input,
   InputRightElement,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
 } from "@chakra-ui/react";
 import { FiSearch } from "react-icons/fi";
 
@@ -71,196 +76,9 @@ export default function Home() {
     onOpen();
   }
 
-  const markdown = DataModal.data;
-
-  if (listProjects == true) {
-    return (
-      <ChakraProvider>
-        <Head>
-          <title>{"I'm Dário Jr"}</title>
-          <meta
-            name="description"
-            content="I'm Dário, a young programmer who loves to learn new things. I am passionate about technology and programming!"
-          />
-          <link rel="icon" href="/github.svg" />
-        </Head>
-
-        <div className={styles.nav}></div>
-        <main className={projects.main}>
-          <div className={projects.row_container}>
-            <HStack marginBottom={1} spacing={1}>
-              <img
-                width={41}
-                src="https://avatars.githubusercontent.com/u/53487868?v=4"
-                alt="Avatar"
-              ></img>{" "}
-              <Heading fontSize={31} noOfLines={1} className={projects.name}>
-                {"Dário Jr"}
-              </Heading>
-              <InputGroup className={projects.searchNavBox}>
-                <Input
-                  maxWidth={120}
-                  variant="unstyled"
-                  placeholder="Search project"
-                />
-                <FiSearch
-                  className={projects.iconSearch}
-                  size={"22px"}
-                ></FiSearch>
-              </InputGroup>
-            </HStack>
-            <Divider></Divider>
-
-            <VStack>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                whileHover={{
-                  scale: 1.2,
-                  rotate: 0,
-                }}
-                whileTap={{
-                  scale: 0.9,
-                  borderRadius: "100%",
-                }}
-              >
-                <Text fontSize={20} className={projects.public_repositories}>
-                  {"Public Repositories"}
-                </Text>
-              </motion.div>
-            </VStack>
-
-            <HStack
-              className={projects.boxIco}
-              marginTop={2}
-              marginBottom={4}
-              spacing={3}
-              marginLeft={1}
-            >
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                whileHover={{
-                  scale: 1.2,
-                  rotate: 0,
-                }}
-                whileTap={{
-                  scale: 0.9,
-                  borderRadius: "100%",
-                }}
-              >
-                <a href="https://github.com/birdra1n">
-                  {" "}
-                  <FaGithub className={projects.icon} size={"20px"}></FaGithub>
-                </a>
-              </motion.div>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                whileHover={{
-                  scale: 1.2,
-                  rotate: 0,
-                }}
-                whileTap={{
-                  scale: 0.9,
-                  borderRadius: "100%",
-                }}
-              >
-                <a href="https://www.linkedin.com/in/ronisd%C3%A1rio-alves-bezerra-j%C3%BAnior-405067234/">
-                  {" "}
-                  <SiLinkedin
-                    className={projects.icon}
-                    size={"20px"}
-                  ></SiLinkedin>
-                </a>
-              </motion.div>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                whileHover={{
-                  scale: 1.2,
-                  rotate: 0,
-                }}
-                whileTap={{
-                  scale: 0.9,
-                  borderRadius: "100%",
-                }}
-              >
-                <a href="https://www.instagram.com/dariojr.ig/">
-                  {" "}
-                  <FaInstagram
-                    className={projects.icon}
-                    size={"20px"}
-                  ></FaInstagram>
-                </a>
-              </motion.div>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                whileHover={{
-                  scale: 1.2,
-                  rotate: 0,
-                }}
-                whileTap={{
-                  scale: 0.9,
-                  borderRadius: "100%",
-                }}
-              >
-                <a href="mailto:dariojunior.dev@gmail.com">
-                  {" "}
-                  <SiGmail className={projects.icon} size={"20px"}></SiGmail>
-                </a>
-              </motion.div>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                whileHover={{
-                  scale: 1.2,
-                  rotate: 0,
-                }}
-                whileTap={{
-                  scale: 0.9,
-                  borderRadius: "100%",
-                }}
-              >
-                <a href="https://t.me/dariojr">
-                  {" "}
-                  <SiTelegram className={projects.icon} size={"20px"} />
-                </a>
-              </motion.div>
-            </HStack>
-
-            <Modal isOpen={isOpen} onClose={onClose} size={"sm"}>
-              <ModalOverlay />
-              <ModalContent className={projects.modal_box}>
-                <ModalHeader className={projects.ModalBody}>
-                  {DataModal.reponame}
-                </ModalHeader>
-                <ModalCloseButton />
-                <ModalBody className={projects.ModalBody}>
-                  <Text className={projects.descriptionModal}>
-                    {DataModal.description}
-                  </Text>
-                </ModalBody>
-
-                <ModalFooter>
-                  <HStack className={projects.footerModal}>
-                    <Text>{DataModal.language}</Text>
-                    <div>
-                      <Button
-                        onClick={() => window.open(DataModal.url)}
-                        variant="ghost"
-                        rightIcon={<ArrowForwardIcon />}
-                      >
-                        Open repo
-                      </Button>
-                    </div>
-                  </HStack>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-
-            <div className={projects.scroll}>
+  const Posts = ()=>{
+    return(
+      <div className={projects.scroll}>
               <SimpleGrid columns={[1, null, 3]} spacing="20px">
                 {DataReq.map((item, itemI) => (
                   <motion.div
@@ -313,6 +131,190 @@ export default function Home() {
                 ))}
               </SimpleGrid>
             </div>
+    )
+  }
+
+  const BoxIcons = () => {
+    return (
+      <HStack
+        className={projects.boxIco}
+        marginTop={2}
+        marginBottom={4}
+        spacing={3}
+        marginLeft={1}
+      >
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 0, scale: 1 }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 0,
+          }}
+          whileTap={{
+            scale: 0.9,
+            borderRadius: "100%",
+          }}
+        >
+          <a href="https://github.com/birdra1n">
+            {" "}
+            <FaGithub className={projects.icon} size={"20px"}></FaGithub>
+          </a>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 0, scale: 1 }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 0,
+          }}
+          whileTap={{
+            scale: 0.9,
+            borderRadius: "100%",
+          }}
+        >
+          <a href="https://www.linkedin.com/in/ronisd%C3%A1rio-alves-bezerra-j%C3%BAnior-405067234/">
+            {" "}
+            <SiLinkedin className={projects.icon} size={"20px"}></SiLinkedin>
+          </a>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 0, scale: 1 }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 0,
+          }}
+          whileTap={{
+            scale: 0.9,
+            borderRadius: "100%",
+          }}
+        >
+          <a href="https://www.instagram.com/dariojr.ig/">
+            {" "}
+            <FaInstagram className={projects.icon} size={"20px"}></FaInstagram>
+          </a>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 0, scale: 1 }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 0,
+          }}
+          whileTap={{
+            scale: 0.9,
+            borderRadius: "100%",
+          }}
+        >
+          <a href="mailto:dariojunior.dev@gmail.com">
+            {" "}
+            <SiGmail className={projects.icon} size={"20px"}></SiGmail>
+          </a>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 0, scale: 1 }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 0,
+          }}
+          whileTap={{
+            scale: 0.9,
+            borderRadius: "100%",
+          }}
+        >
+          <a href="https://t.me/dariojr">
+            {" "}
+            <SiTelegram className={projects.icon} size={"20px"} />
+          </a>
+        </motion.div>
+      </HStack>
+    );
+  };
+
+
+  if (listProjects == true) {
+    return (
+      <ChakraProvider>
+        <Head>
+          <title>{"I'm Dário Jr"}</title>
+          <meta
+            name="description"
+            content="I'm Dário, a young programmer who loves to learn new things. I am passionate about technology and programming!"
+          />
+          <link
+            rel="icon"
+            href="https://images2.imgbox.com/bf/61/jhyKvIs2_o.gif"
+          />
+        </Head>
+
+        <div className={styles.nav}></div>
+        <main className={projects.main}>
+          <div className={projects.row_container}>
+            <HStack marginBottom={1} spacing={1}>
+              <img
+                width={41}
+                src="https://avatars.githubusercontent.com/u/53487868?v=4"
+                alt="Avatar"
+              ></img>{" "}
+              <Heading fontSize={31} noOfLines={1} className={projects.name}>
+                {"Dário Jr"}
+              </Heading>
+              <BoxIcons />
+            </HStack>
+
+            <Modal isOpen={isOpen} onClose={onClose} size={"sm"}>
+              <ModalOverlay />
+              <ModalContent className={projects.modal_box}>
+                <ModalHeader className={projects.ModalBody}>
+                  {DataModal.reponame}
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody className={projects.ModalBody}>
+                  <Text className={projects.descriptionModal}>
+                    {DataModal.description}
+                  </Text>
+                </ModalBody>
+
+                <ModalFooter>
+                  <HStack className={projects.footerModal}>
+                    <Text>{DataModal.language}</Text>
+                    <div>
+                      <Button
+                        onClick={() => window.open(DataModal.url)}
+                        variant="ghost"
+                        rightIcon={<ArrowForwardIcon />}
+                      >
+                        Open repo
+                      </Button>
+                    </div>
+                  </HStack>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+
+            <Tabs colorScheme={'#FFCB26'}>
+              <TabList color={'#FFC200'} >
+                <Tab color={'#FFC200'} >All</Tab>
+                <Tab>Finished releases</Tab>
+                <Tab>Beta releases</Tab>
+              </TabList>
+              <TabPanels >
+                <TabPanel>
+                  <Posts></Posts>
+                </TabPanel>
+                <TabPanel>
+                  <p></p>
+                </TabPanel>
+                <TabPanel>
+                  <p></p>
+                </TabPanel>
+              </TabPanels>
+
+             
+            </Tabs>
+
+            
           </div>
         </main>
 
@@ -346,39 +348,49 @@ export default function Home() {
             name="description"
             content="I'm Dário, a young programmer who loves to learn new things. I am passionate about technology and programming!"
           />
-          <link rel="icon" href="/github.svg" />
+          <link
+            rel="icon"
+            href="https://images2.imgbox.com/bf/61/jhyKvIs2_o.gif"
+          />
         </Head>
 
         <div className={styles.nav}></div>
         <main className={styles.main}>
           <div className={styles.hello_div}>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 0.8,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.4,
+            <HStack>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={{
+                  hidden: {
+                    scale: 0.8,
+                    opacity: 0,
                   },
-                },
-              }}
-            >
-              <Heading
-                position={"relative"}
-                as="h1"
-                fontSize={31}
-                noOfLines={1}
-                className={styles.hello}
+                  visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                      delay: 0.4,
+                    },
+                  },
+                }}
               >
-                Hello, I’m
-              </Heading>
-            </motion.div>
+                <Heading
+                  position={"relative"}
+                  as="h1"
+                  fontSize={31}
+                  noOfLines={1}
+                  className={styles.hello}
+                >
+                  Hello, I’m
+                </Heading>
+              </motion.div>
+              <img
+                width={40}
+                src="https://images2.imgbox.com/bf/61/jhyKvIs2_o.gif"
+                alt="Avatar"
+              ></img>{" "}
+            </HStack>
           </div>
           <motion.div
             initial="hidden"
@@ -450,9 +462,9 @@ export default function Home() {
             <Button
               rightIcon={<ArrowForwardIcon />}
               borderRadius={4}
-              colorScheme="blue"
+              colorScheme="orange"
               size="sm"
-              bgColor={"blue.900"}
+              bgColor={"#F7C112"}
               className={styles.btn_hello}
               onClick={() => SetListProjects(true)}
             >
