53fcad5adeeeb6214d3f88b04491a07bc9906c31
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-06-25 10:58:07 -0300

    Update scrollApps.tsx

--- components/home/scrollApps.tsx
+++ components/home/scrollApps.tsx
@@ -14,14 +14,23 @@ const ScrollApps = () => {
         const { data, error } = await supabase
             .from('apps')
             .select()
-            .order('id')
-            .limit(3);
+            .order('id');
 
-        if (!error) {
+        const { data: cardStyles, error: errorStyle } = await supabase.from('styleAppCard').select().order('id')
+
+
+        if (!error || !errorStyle) {
             setApps(Object(data));
+            // Mapear estilos para cada aplicativo
+            const appsWithStyles = data?.map(app => {
+                const stylesForApp = cardStyles.filter(style => style.appId === app.id);
+                return { ...app, style: stylesForApp[0] };
+            });
+            setApps(appsWithStyles);
             setIsloading(false);
         }
     };
+
 
     useEffect(() => {
         fetchData();
@@ -32,14 +41,14 @@ const ScrollApps = () => {
             {isLoading ? <center className="justify-center">
                 <Spinner className="mt-[25vh]" color="warning" />
             </center> : <div className="flex justify-center p-10 w-full overflow-x-auto hide-scrollbar">
-                {apps.map((item: { title: string, subtitle: string, platforms: any }, index) => (
+                {apps.map((item: { title: string, subtitle: string, platforms: any, style: { mt_cardImage: number } }, index) => (
                     <div key={index}>
-                        <Card isFooterBlurred className="ml-[33px] h-[300px] min-w-[330px] md:min-w-[400px] max-w-[330px] sm:min-w-[330px] max-w-[380px]">
+                        <Card style={{ borderRadius: 10 }} isFooterBlurred className="ml-[33px] h-[300px] min-w-[330px] md:min-w-[400px] max-w-[330px] sm:min-w-[330px] max-w-[380px]">
                             <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                                <p className="text-tiny text-black/60 uppercase font-bold">{item?.title}</p>
                                 <h4 className="text-black/60 font-medium text-xl">{item?.subtitle}</h4>
                             </CardHeader>
                             <Image
+                                style={{ marginTop: item?.style?.mt_cardImage }}
                                 removeWrapper
                                 alt="Relaxing app background"
                                 className="z-0 w-full h-full object-cover"
