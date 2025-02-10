c9eff376207396cdc68f9d32b6238b986f6af5a4
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-02-10 15:43:14 -0300

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -47,7 +47,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
       aria-label={isSelected ? "Switch to dark mode" : "Switch to light mode"}
       {...getBaseProps({
         className: clsx(
-          "px-px transition-opacity hover:opacity-80 cursor-pointer mb-[5.5]",
+          "px-px transition-opacity hover:opacity-80 cursor-pointer",
           className,
           classNames?.base,
         ),
