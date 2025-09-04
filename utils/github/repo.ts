16c3e5a87fb3ec048b86f2ec49a03611e2685081
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-09-04 13:01:14 -0300

    feat(github): ordenar repositórios pelo número de habilidades em ordem decrescente
    Adiciona funcionalidade de ordenação na recuperação de repositórios do GitHub para priorizar os que possuem mais habilidades
    
    Melhora a ordem de exibição para uma melhor experiência do usuário

--- utils/github/repo.ts
+++ utils/github/repo.ts
@@ -5,7 +5,7 @@ export interface GitHubRepoWithSkills extends GitHubRepo {
 }
 
 const getRepos = async (): Promise<any[]> => {
-    const response = await fetch("api/repos/github"); // rota que chama a Edge Function
+    const response = await fetch("api/repos/github");
 
     if (!response.ok) {
         throw new Error(`Erro ao buscar repositórios: ${response.statusText}`);
@@ -13,8 +13,17 @@ const getRepos = async (): Promise<any[]> => {
 
     const data = await response.json();
 
-    // data.repos contém os repositórios
-    return data.repos as GitHubRepoWithSkills[];
+    const reposWithSkills = data.repos as GitHubRepoWithSkills[];
+
+    // Ordena os repositórios com base na quantidade de skills em ordem decrescente
+    const sortedRepos = reposWithSkills.sort((a, b) => {
+        const aSkillsCount = (a.skills && a.skills.length) || 0;
+        const bSkillsCount = (b.skills && b.skills.length) || 0;
+
+        return bSkillsCount - aSkillsCount;
+    });
+
+    return sortedRepos;
 };
 
 export default getRepos;
